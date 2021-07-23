c78b4a9b920f1563ee98940ffe47fecc672207ba
kishansinha181 <54812321+kishansinha181@users.noreply.github.com>
2021-07-23 19:11:54 +0530

    Time: 4 ms (75.25%), Space: 7.9 MB (38.99%) - LeetHub

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,14 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
-        ans.push_back(nums);
-        while(next_permutation(nums.begin(),nums.end()))
-        {
-            ans.push_back(nums);
+    vector<vector<int>> ans;
+    
+    void permute(vector<int> & arr, int i ){
+        int n = arr.size();
+        if(n-1 == i){
+            ans.push_back(arr);
+            return;
         }
         
+        
+            for(int j = i; j < n; j++){
+                swap(arr[j], arr[i]);
+                permute(arr,i+1);
+                swap(arr[j],arr[i]);
+            }
+        
+        
+        
+            
+        
+    }
+    
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        // vector<vector<int>> ans;
+        
+        permute(nums,0);
+        
         return ans;
+        
+        
     }
 };
